5e3dd76127466385d44dfdbb5906bf397f67c69c
OlenaAndrushchenko <olena.anru.anru@gmail.com>
2024-08-26 16:38:27 +0200

    created Sign In form component, edited form field component

--- src/components/base/FormField.vue
+++ src/components/base/FormField.vue
@@ -18,7 +18,7 @@
 
 <template>
     <div class="flex flex-col items-start gap-2 w-full">
-      <label :for="id" class="text-grey text-sm font-semibold font-rubik leading-tight">
+      <label :for="id" class="text-grey text-sm font-semibold font-rubik">
         {{ label }}
       </label>
       <input 
@@ -27,7 +27,7 @@
         :placeholder="placeholder"
         :value="modelValue"
         @input="updateValue"
-        class="w-full p-4 bg-purple-500 text-purple-200 text-lg font-normal font-inter leading-[27px] rounded-lg border-2 border-transparent focus:border-pink hover:border-pink focus:text-grey placeholder:text-purple-200 cursor-pointer"
+        class="w-full p-4 bg-purple-500 text-purple-200 text-lg font-normal font-inter rounded-lg border-2 border-transparent focus:border-pink hover:border-pink focus:text-grey placeholder:text-purple-200 cursor-pointer"
       />
     </div>
 </template>
