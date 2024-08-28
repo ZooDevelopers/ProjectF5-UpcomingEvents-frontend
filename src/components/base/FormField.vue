8d5299a7c0a0808fedd938288e5574bd05d3e893
OlenaAndrushchenko <olena.anru.anru@gmail.com>
2024-08-28 13:07:11 +0200

    added to form field managing required fields, improved responsiveness of the modal

--- src/components/base/FormField.vue
+++ src/components/base/FormField.vue
@@ -7,6 +7,7 @@
     type: String,
     placeholder: String,
     modelValue: String,
+    required: Boolean,
     error: String,
     });
 
@@ -18,9 +19,10 @@
 </script>
 
 <template>
-    <div class="flex flex-col items-start gap-2 w-full">
+    <div class="flex flex-col items-start gap-1.5 w-full">
       <label :for="id" class="text-grey text-sm font-semibold font-rubik">
         {{ label }}
+        <span v-if="required" class="text-red">*</span>
       </label>
       <input 
         :id="id"
@@ -28,7 +30,8 @@
         :placeholder="placeholder"
         :value="modelValue"
         @input="updateValue"
-        class="w-full p-4 bg-purple-500 text-purple-200 text-lg font-normal font-inter rounded-lg border-2 border-transparent focus:border-pink hover:border-pink focus:text-grey placeholder:text-purple-200 cursor-pointer"
+        :required="required"
+        class="w-full p-3 bg-purple-500 text-purple-200 text-base font-normal font-inter rounded-lg border-2 border-transparent focus:border-pink hover:border-pink focus:text-grey placeholder:text-purple-200 cursor-pointer"
       />
       <p v-if="error" class="text-red text-sm font-normal font-inter">{{ error }}</p>
     </div>
