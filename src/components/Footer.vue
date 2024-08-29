0d22b8aa09db874a7c5e80dff47e738a75d8976a
Julia <musicaparatodo770@gmail.com>
2024-08-29 11:08:03 +0200

    I add tailwind to the footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -12,17 +12,7 @@
 
 <style scoped>
 
-/* .footer {
-    background-color:#1F1C2F; 
-    padding: 5px; 
-    color: #ECBDF2; 
-    text-align: center; 
-    border-top: 2px solid #40E0D0;
-    width: 100%; 
-    height: 35px;
-    margin-top: auto;   
-    left: 0;
-  } */
+
   
 
 
