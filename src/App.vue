e1da66eb35e4f44d2be43a5f3f74d0ab8253f82d
OlenaAndrushchenko <olena.anru.anru@gmail.com>
2024-09-04 19:40:34 +0200

    files organization

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <script setup>
-import Header from './components/Header.vue'
-import Footer from './components/Footer.vue';
+import Header from './components/global/Header.vue'
+import Footer from './components/global/Footer.vue';
 </script>
 
 <template>
