d33ef537c20921ce01504a474bcbef0db5144d85
OlenaAndrushchenko <olena.anru.anru@gmail.com>
2024-09-04 15:20:24 +0200

    added icons and some styles

--- src/components/slider/HomeSlider.vue
+++ src/components/slider/HomeSlider.vue
@@ -4,9 +4,9 @@ import { Swiper } from "swiper";
 import { Navigation } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/navigation";
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
-import { useEventStore } from '@/stores/events';
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { faArrowRight, faLocationDot, faCalendar } from "@fortawesome/free-solid-svg-icons";
+import { useEventStore } from "@/stores/events";
 
 const navigationPrevRef = ref(null);
 const navigationNextRef = ref(null);
@@ -27,9 +27,9 @@ const updateButtonState = () => {
 onMounted(async () => {
   try {
     await eventStore.setEvents();
-    events.value = eventStore.events.filter(event => event.getIs_Featured());
+    events.value = eventStore.events.filter((event) => event.getIs_Featured());
 
-    await nextTick(); 
+    await nextTick();
 
     swiperInstance.value = new Swiper(".mySwiper", {
       modules: [Navigation],
@@ -58,18 +58,19 @@ const openModal = (event) => {
   <div class="relative">
     <div class="swiper mySwiper">
       <div class="swiper-wrapper">
-        <swiper-slide 
-          v-for="event in events" 
-          :key="event.title" 
+        <swiper-slide
+          v-for="event in events"
+          :key="event.title"
           class="swiper-slide slide-temporary cursor-pointer"
-          @click="openModal(event)">
-          <div class="slide-content flex flex-col">
-            <div class="p-5 bg-purple-600 max-w-[600px] rounded-[20px]">
-              <span class="flex flex-row gap-5 text-grey">
-                <p>{{ event.getDate() }}</p>
-                <p>{{ event.getLocation() }}</p>
+          @click="openModal(event)"
+        >
+          <div class="slide-content flex flex-col max-w-[1290px] mx-auto py-16">
+            <div class="p-5 bg-purple-600 max-w-[600px] rounded-[20px] flex flex-col gap-5">
+              <span class="flex gap-8 text-grey font-rubik">
+                <p><FontAwesomeIcon :icon="faCalendar" class="text-xl text-pink mr-1" /> {{ event.getDate() }}</p>
+                <p><FontAwesomeIcon :icon="faLocationDot" class="text-xl text-pink mr-1" /> {{ event.getLocation() }}</p>
               </span>
-              <h3 class="text-peach-500 text-3xl">{{ event.getTitle() }}</h3>
+              <h3 class="text-peach-500 text-3xl font-rubik font-semibold uppercase">{{ event.getTitle() }}</h3>
             </div>
           </div>
         </swiper-slide>
@@ -78,8 +79,11 @@ const openModal = (event) => {
 
     <button
       ref="navigationPrevRef"
-      :class="[ 
-        'absolute top-1/2 -translate-y-1/2 left-4 z-10 w-12 h-12 rounded-full flex items-center justify-center', isBeginning ? 'bg-peach-600/60 cursor-not-allowed text-purple-700/50' : 'bg-peach-600 text-purple-700'
+      :class="[
+        'absolute top-1/2 -translate-y-1/2 left-4 z-10 w-12 h-12 rounded-full flex items-center justify-center',
+        isBeginning
+          ? 'bg-peach-600/60 cursor-not-allowed text-purple-700/50'
+          : 'bg-peach-600 text-purple-700'
       ]"
     >
       <FontAwesomeIcon :icon="faArrowRight" class="text-xl rotate-180" />
@@ -87,21 +91,20 @@ const openModal = (event) => {
     <button
       ref="navigationNextRef"
       :class="[
-        'absolute top-1/2 -translate-y-1/2 right-4 z-10 w-12 h-12 rounded-full flex items-center justify-center', isEnd ? 'bg-peach-600/60 cursor-not-allowed text-purple-700/50' : 'bg-peach-600 text-purple-700' 
+        'absolute top-1/2 -translate-y-1/2 right-4 z-10 w-12 h-12 rounded-full flex items-center justify-center',
+        isEnd
+          ? 'bg-peach-600/60 cursor-not-allowed text-purple-700/50'
+          : 'bg-peach-600 text-purple-700'
       ]"
     >
       <FontAwesomeIcon :icon="faArrowRight" class="text-xl" />
     </button>
   </div>
 </template>
-  
 
 <style>
 .slide-temporary {
-    height: 750px;
+  height: 750px;
 }
 
-/* .slide-content {
-
-} */
 </style>
